f126fddceeb9c30dcbce534826338890ed2463d2
hisho <hisho.web@gmail.com>
2023-06-23 03:19:16 +0900

    ✨ create pokemon card component

--- src/feature/pokemon/pokemon-card/pokemon-card.tsx
+++ src/feature/pokemon/pokemon-card/pokemon-card.tsx
@@ -1,9 +1,16 @@
 import { graphql } from '@/lib/gql'
 import { PokemonCard_PokemonFragment } from '@/lib/gql/graphql'
+import Image from 'next/image'
 
 export const PokemonCard_pokemon = graphql(`
   fragment PokemonCard_pokemon on Pokemon {
     name
+    image
+    evolutions {
+      name
+      image
+      id
+    }
   }
 `)
 
@@ -12,5 +19,22 @@ type Props = {
 }
 
 export const PokemonCard = ({ pokemon }: Props) => {
-  return <div>{pokemon.name}</div>
+  console.log(pokemon.evolutions)
+  return (
+    <div>
+      <header>
+        <div className={'relative'} style={{ aspectRatio: 16 / 9 }}>
+          {pokemon.image && (
+            <Image
+              alt={''}
+              className={'object-contain'}
+              fill
+              src={pokemon.image}
+            />
+          )}
+        </div>
+      </header>
+      <div>{pokemon.name}</div>
+    </div>
+  )
 }
