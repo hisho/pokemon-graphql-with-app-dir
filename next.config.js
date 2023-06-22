9b9f616f4fe7eae130021a81967d7a68ff6bcfbf
hisho <hisho.web@gmail.com>
2023-06-23 03:17:53 +0900

    🔧 update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    domains: ['img.pokemondb.net'],
+  },
+}
 
 module.exports = nextConfig
